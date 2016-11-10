c264144b665d0b09f703b2eecd5cbf6b056024ab
Yaswardhan <yaswardhan@spinircle.com>
2016-11-10 12:17:55 +0530

    added WYPopOver

---
+++ SPWYPopoverController/SPWYPopoverController/SPWYPopoverController.h
@@ -0,0 +1,19 @@
+//
+//  SPWYPopoverController.h
+//  SPWYPopoverController
+//
+//  Created by Yaswardhan on 10/11/16.
+//  Copyright © 2016 Yaswardhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPWYPopoverController.
+FOUNDATION_EXPORT double SPWYPopoverControllerVersionNumber;
+
+//! Project version string for SPWYPopoverController.
+FOUNDATION_EXPORT const unsigned char SPWYPopoverControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPWYPopoverController/PublicHeader.h>
+
+
